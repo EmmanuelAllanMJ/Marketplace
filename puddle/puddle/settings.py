9c2e31f3ced51be48b7a0006f652f009bb19d6fb
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-08-07 18:44:57 +0530

    Debug false

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 # security.W016
 CSRF_COOKIE_SECURE = True
